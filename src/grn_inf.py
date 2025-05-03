c8c9bbb291f6a0db91e9858d146181b314bbb490
Adam Stcklov <adamkutak@gmail.com>
2025-05-03 19:28:58 -0400

    test

--- src/grn_inf.py
+++ src/grn_inf.py
@@ -16,7 +16,7 @@ from src.models.components.plotting import compute_global_jacobian, plot_auprs, 
 DEFAULT_DATA_PATH = "data/"
 DEFAULT_DATASET_TYPE = "Renge"
 DEFAULT_MODEL_TYPE = "sf2m"
-DEFAULT_N_STEPS = 1000
+DEFAULT_N_STEPS = 100
 DEFAULT_BATCH_SIZE = 64
 DEFAULT_LR = 3e-3
 DEFAULT_ALPHA = 0.1
