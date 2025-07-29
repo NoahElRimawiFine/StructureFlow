0789fd8b668cea91f7c744828e3237b4c87bb478
Adam Stcklov <adamkutak@gmail.com>
2025-07-29 14:21:18 -0400

    adding rf scaling hparam search

--- src/models/rf_module.py
+++ src/models/rf_module.py
@@ -30,7 +30,7 @@ class ReferenceFittingModule(pl.LightningModule):
         # Add a dummy parameter so that configure_optimizers has something to optimize.
         self.dummy_param = torch.nn.Parameter(torch.tensor(0.0))
 
-    def fit_model(self, adatas, kos):
+    def fit_model(self, adatas, kos, also_wt=True):
         """Fits the reference model using both knockout and wild-type data.
 
         This method replicates your original 'train' method.
@@ -45,13 +45,14 @@ class ReferenceFittingModule(pl.LightningModule):
         )
         self.estimator.fit(print_iter=100, alg="alternating", update_couplings_iter=250)
 
-        print("Training reference model with wild type data only...")
-        self.estimator_wt = rf.Estimator(
-            [adatas[i] for i in wt_idx], [kos[i] for i in wt_idx], **self.options
-        )
-        self.estimator_wt.fit(
-            print_iter=100, alg="alternating", update_couplings_iter=250
-        )
+        if also_wt:
+            print("Training reference model with wild type data only...")
+            self.estimator_wt = rf.Estimator(
+                [adatas[i] for i in wt_idx], [kos[i] for i in wt_idx], **self.options
+            )
+            self.estimator_wt.fit(
+                print_iter=100, alg="alternating", update_couplings_iter=250
+            )
 
     def fit_model_with_holdout(self, adatas, kos, left_out_time):
         """Fits the reference model using both knockout and wild-type data, taking into account a
