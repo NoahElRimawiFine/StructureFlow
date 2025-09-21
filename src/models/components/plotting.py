47cc4d25d4bc422603e69eddbada666220d80943
Noah El Rimawi-Fine <noah.elrimawi-fine@mail.mcgill.ca>
2025-09-21 18:34:48 -0400

    a

--- src/models/components/plotting.py
+++ src/models/components/plotting.py
@@ -538,6 +538,7 @@ def log_causal_graph_matrices(A_estim=None, W_v=None, A_true=None, logger=None, 
         A_estim_plot = maskdiag(A_estim) if mask_diagonal else A_estim
 
     W_v_plot = maskdiag_np(W_v.T) if mask_diagonal else W_v.T
+    print(W_v_plot)
     A_true_plot = maskdiag_np(A_true) if mask_diagonal else A_true
 
     # --- A_estim ---
