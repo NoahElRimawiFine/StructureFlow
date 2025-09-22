3be705dc146176bce02470f33aec6642d6b4bdc4
Noah El Rimawi-Fine <noah.elrimawi-fine@mail.mcgill.ca>
2025-09-22 13:39:19 -0400

    a

--- src/models/components/sf2m_ngm1.py
+++ src/models/components/sf2m_ngm1.py
@@ -148,20 +148,20 @@ class SF2MNGM(nn.Module):
             self.knockout_masks.append(mask_i)
 
         self.dims = [self.n_genes, 32, 32, 1]
-        # self.func_v = MLPODEFKO(
-        #     dims=self.dims, GL_reg=GL_reg, bias=True, knockout_masks=self.knockout_masks
-        # ).to(self.device)
-        self.func_v = BayesianDrift(
-            dims=self.dims, 
-            n_ens=100, 
-            deepens=True, 
-            time_invariant=True, 
-            k_hidden=dyn_hidden,
-            alpha=self.dyn_alpha,
-            knockout_masks=self.knockout_masks,
-            step=0,
-            hyper="mlp",
+        self.func_v = MLPODEFKO(
+            dims=self.dims, GL_reg=GL_reg, bias=True, knockout_masks=self.knockout_masks
         ).to(self.device)
+        # self.func_v = BayesianDrift(
+        #     dims=self.dims, 
+        #     n_ens=100, 
+        #     deepens=True, 
+        #     time_invariant=True, 
+        #     k_hidden=dyn_hidden,
+        #     alpha=self.dyn_alpha,
+        #     knockout_masks=self.knockout_masks,
+        #     step=0,
+        #     hyper="mlp",
+        # ).to(self.device)
 
         self.score_net = CONDMLP(
             d=self.n_genes,
